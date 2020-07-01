97460278d5ee4ad54dad9363890c1fd8e7c5ed85
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-06-30 20:18:44 -0700

    react-tilt library added to project

--- face-recognition/src/components/Logo/Logo.js
+++ face-recognition/src/components/Logo/Logo.js
@@ -2,7 +2,9 @@ import React from "react";
 
 const Logo = () => {
     return(
-        <h1>LogoTime</h1>
+        <div className='ma4 mt0'>
+            <h1>LogoTime</h1>
+        </div>
     )
 }
 
