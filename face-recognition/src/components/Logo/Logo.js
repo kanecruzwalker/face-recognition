d7a03834157705fce961b535e0ec5bc82d4bf51d
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-06-30 20:27:06 -0700

    added css to logo component

--- face-recognition/src/components/Logo/Logo.js
+++ face-recognition/src/components/Logo/Logo.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Tilt from 'react-tilt';
+import "./Logo.css";
 
 const Logo = () => {
     return(
         <div className='ma4 mt0'>
-            <Tilt className="Tilt" options={{ max : 25 }} style={{ height: 150, width: 150 }} >
+            <Tilt className="Tilt br2 shadow-2" options={{ max : 45 }} style={{ height: 150, width: 150 }} >
             <div className="Tilt-inner"> 
             👽 
             <h1>LogoTime</h1>
