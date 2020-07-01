6a427a88d07378a0c54f49e9a57ea3bd48e55cc2
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-06-30 20:35:36 -0700

    logo component using image and react-tilt

--- face-recognition/src/components/Logo/Logo.js
+++ face-recognition/src/components/Logo/Logo.js
@@ -1,15 +1,15 @@
 import React from "react";
 import Tilt from 'react-tilt';
 import "./Logo.css";
+import fox from "./fox.png"
 
 const Logo = () => {
     return(
         <div className='ma4 mt0'>
             <Tilt className="Tilt br2 shadow-2" options={{ max : 45 }} style={{ height: 150, width: 150 }} >
-            <div className="Tilt-inner"> 
-            👽 
-            <h1>LogoTime</h1>
-            </div>
+                <div className="Tilt-inner"> 
+                    <img src={fox} alt="logo"/>
+                </div>
             </Tilt>
 
         </div>
