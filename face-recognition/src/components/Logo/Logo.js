01403355f1197e2c1655e8d54ed151b43eb2a0db
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-06-30 20:22:24 -0700

    implemented tilt boiler plate

--- face-recognition/src/components/Logo/Logo.js
+++ face-recognition/src/components/Logo/Logo.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Tilt from 'react-tilt';
 
 const Logo = () => {
     return(
         <div className='ma4 mt0'>
+            <Tilt className="Tilt" options={{ max : 25 }} style={{ height: 150, width: 150 }} >
+            <div className="Tilt-inner"> 
+            👽 
             <h1>LogoTime</h1>
+            </div>
+            </Tilt>
+
         </div>
     )
 }
