a1fe1f5123813ce408d0a0a654106bd6198771bf
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-06-30 22:17:43 -0700

    particles is the background of webpage now

--- face-recognition/src/App.js
+++ face-recognition/src/App.js
@@ -4,9 +4,27 @@ import Navigation from "./components/Navigation/Navigation"
 import Rank from "./components/Rank/Rank"
 import Logo from "./components/Logo/Logo"
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm"
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
+
+
 function App() {
   return (
     <div className="App">
+      <Particles className='particles'
+        params={{particlesOptions}} 
+      />
       <Navigation/>
       <Logo/>
       <Rank/>
