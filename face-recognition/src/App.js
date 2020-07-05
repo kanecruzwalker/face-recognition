310e3e125da5851374f68b3c9c894744b43b0bdb
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-07-05 13:41:27 -0700

    moved clarifai key to server, to keep key more secure

--- face-recognition/src/App.js
+++ face-recognition/src/App.js
@@ -5,12 +5,12 @@ import Rank from "./components/Rank/Rank"
 import Logo from "./components/Logo/Logo"
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm"
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
+
 import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 import SignIn from "./components/SignIn/SignIn"
 import Register from "./components/Register/Register"
 
-const app = new Clarifai.App({apiKey: '95d1e072cc6844acb57382cdd12dbe35'});
+
 
 const particlesOptions = {
   particles: {
@@ -82,10 +82,15 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    app.models
-      .predict(
-        'a403429f2ddf4b49b307e318f00e528b',
-        this.state.input)
+    fetch('http://localhost:3000/imageurl', {
+            method: 'post',
+            headers: {
+              'Content-Type': 'application/json',},
+            body: JSON.stringify({
+              input: this.state.input
+            })
+          })
+          .then(response => response.json())
       .then(response => {
         if (response) {
           fetch('http://localhost:3000/image', {
